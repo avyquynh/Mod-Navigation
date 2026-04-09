8cbdeeeb68c1c11f1c4bde4492ae134e269c7b8d
avyquynh <avyqchung05@gmail.com>
2026-04-09 13:31:21 -0700

    sure

--- target/debug/deps/solar_spell_library-61a07126633dba75.d
+++ target/debug/deps/solar_spell_library-61a07126633dba75.d
@@ -1,8 +1,8 @@
-/Users/avyquynh/Documents/GIT/Mod-Navigation/target/debug/deps/solar_spell_library-61a07126633dba75.d: src/main.rs /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/file_view.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/base.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/folder_view.html
+/Users/avyquynh/Documents/GIT/Mod-Navigation/target/debug/deps/solar_spell_library-61a07126633dba75.d: src/main.rs /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/base.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/file_view.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/folder_view.html
 
-/Users/avyquynh/Documents/GIT/Mod-Navigation/target/debug/deps/solar_spell_library-61a07126633dba75: src/main.rs /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/file_view.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/base.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/folder_view.html
+/Users/avyquynh/Documents/GIT/Mod-Navigation/target/debug/deps/solar_spell_library-61a07126633dba75: src/main.rs /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/base.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/file_view.html /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/folder_view.html
 
 src/main.rs:
-/Users/avyquynh/Documents/GIT/Mod-Navigation/templates/file_view.html:
 /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/base.html:
+/Users/avyquynh/Documents/GIT/Mod-Navigation/templates/file_view.html:
 /Users/avyquynh/Documents/GIT/Mod-Navigation/templates/folder_view.html:
